7ff64b4f7848aa7344250579ab2da34074bb708d
audioryan9 <55265265+audioryan9@users.noreply.github.com>
2019-09-27 18:40:24 -0400

    sdfasf

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function randomFlip(){
+    document.querySelector("#vegasDeck").innerHTML= Math.floor(Math.random()* 10)
+}
+function randomFlip2(){
+    document.querySelector("#renoDeck").innerHTML= Math.floor(Math.random()* 10)
+}
